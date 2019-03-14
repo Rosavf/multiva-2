2326f091def98454e63ed6dff792e53304896f01
HardCoreWare <bernardocodu@gmail.com>
2019-03-14 10:35:40 -0600

    ajustando estilos

--- app/views/editable.php
+++ app/views/editable.php
@@ -9,9 +9,7 @@
                                         <form class="form-horizontal" role="form">
 
                                             <div class="form-group">
-                                                <div class="col-sm-10">
-                                                    <input class="btn btn-block btn-success" type="button" value="ACTUALIZAR DATOS" id="btnUpdate">
-                                                </div>
+                                                <input class="btn btn-block btn-success" type="button" value="ACTUALIZAR DATOS" id="btnUpdate">
                                             </div>
 
                                         </form>
