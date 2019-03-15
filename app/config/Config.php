a5869c7b8452c3f188a144c561f819618dd78f8b
HardCoreWare <bernardocodu@gmail.com>
2019-03-15 12:32:56 -0600

    preparando nuevo esquema

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,7 +1,7 @@
 <?php
 
-$configJson=file_get_contents('../app/config/config.json');
-$config=json_decode($configJson,true);
-define('URL',$config['url']);
+    $configJson=file_get_contents('../app/config/config.json');
+    $config=json_decode($configJson,true);
+    define('URL',$config['url']);
 
 ?>
